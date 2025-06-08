f6a3294a2bf3c7c2c51140502d85e6df997b0343
Kurtsley <73447098+Kurtsley@users.noreply.github.com>
2025-06-08 18:56:27 -0500

    Changed window title and startup size

--- conf.lua
+++ conf.lua
@@ -1,18 +1,17 @@
 -- Conf file
 function love.conf(t)
-    t.window.title = "MS"
+    t.window.title = "MinesweeperXP Classic"
     t.window.icon = "assets/icons/icon-small.png"
-    t.window.width = 288
-    t.window.height = 352
+    t.window.width = 176
+    t.window.height = 264
     t.modules.joystick = false
     t.modules.physics = false
 
     t.releases = {
         title = "ms-love",
-        version = "1.0.0",
+        version = "1.1.1",
         author = "Kurtsley",
         email = "kurtsley@gmail.com",
         description = "Minesweeper with Love",
-        homepage = "www.example.com",
     }
 end
